88ca5147b833212fa2981cc0084eff7a0c9ddcb0
Bihairui <bihairui@qq.com>
2024-04-09 13:13:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const http = require('http');
-const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
